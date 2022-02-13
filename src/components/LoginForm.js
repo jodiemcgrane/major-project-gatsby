ef2c62d9c1bfa01a93dc237088e74aa598d1680f
Jodie Mcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2022-02-13 14:56:02 +0000

    Setup LoginForm states and stored token in localstorage

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,49 @@
 import React from "react"
+import axios from "axios"
 import { useState } from "react"
 
 //Components
 import { FormControl, Input, Button } from "@atlas-design-system/react"
 
 const LoginForm = () => {
+  const [form, setForm] = useState({})
+
+  const handleForm = e => {
+    setForm(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const submitForm = () => {
+    axios
+      .post("http://internport-api.herokuapp.com/auth/local", {
+        identifier: form.identifier,
+        password: form.password,
+      })
+      .then(response => {
+        let token = response.data.jwt
+        localStorage.setItem("Token", token)
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
       <FormControl id="myFormControl" label="Username">
-        <Input id="myInput" />
+        <Input id="myInput" name="identifier" onChange={handleForm} />
       </FormControl>
       <FormControl id="myFormControl" label="Password">
-        <Input id="myInput" type="password" />
+        <Input
+          id="myInput"
+          type="password"
+          name="password"
+          onChange={handleForm}
+        />
       </FormControl>
-      <Button
-          appearance="primary"
-          type="submit"
-        >
-          Login
-        </Button>
+      <Button appearance="primary" type="submit" onClick={submitForm}>
+        Login
+      </Button>
     </>
   )
 }
