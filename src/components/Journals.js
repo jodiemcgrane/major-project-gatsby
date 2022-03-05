71d1c8a116cf8ad74d6e7ce53421789993bbfb47
Jodie Mcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2022-03-05 10:38:10 +0000

    Updated Journals component

--- src/components/Journals.js
+++ src/components/Journals.js
@@ -5,11 +5,11 @@ import Journal from "./Journal"
 
 const Journals = ({ journals }) => {
   return (
-    <div>
+    <>
       {journals.map((journal, index) => {
         return <Journal key={journal.id} index={index} {...journal} />
       })}
-    </div>
+    </>
   )
 }
 
