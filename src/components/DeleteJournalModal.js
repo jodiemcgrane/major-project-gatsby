2ac87fb5d891124e370123c6dccc2a87f67bbb62
Jodie Mcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2022-03-18 17:44:16 +0000

    DeleteJournalModal styling

--- src/components/DeleteJournalModal.js
+++ src/components/DeleteJournalModal.js
@@ -1,28 +1,63 @@
 import React from "react"
 
+import axios from "axios"
+
 //Atlas Components
 import {
   Typography,
   ButtonGroup,
   Button,
   Modal,
+  TrashIcon,
 } from "@atlas-design-system/react"
 
 const DeleteJournalModal = ({ journal, open, handleClose }) => {
-    console.log(open)
   return (
     <>
       <Modal
         hidden={open}
         onDismiss={handleClose}
         actionButtons={
-          <ButtonGroup align="right">
-            <Button appearance="primary">Primary</Button>
-            <Button appearance="secondary">Secondary</Button>
-          </ButtonGroup>
+          <div className="modal-buttons">
+            <ButtonGroup align="right">
+              <Button appearance="primary">Confirm</Button>
+              <Button appearance="secondary" onClick={handleClose}>
+                Cancel
+              </Button>
+            </ButtonGroup>
+          </div>
         }
+        title="Delete Journal"
+        size="small"
       >
-        Hello Modal
+        <div className="modal-spacing">
+          <div className="row text-display">
+            <TrashIcon width="125" />
+          </div>
+
+          <div className="row text-display">
+            <div className="col-md-6 modal-heading">
+              <Typography variant="displayText2">Delete journal?</Typography>
+            </div>
+          </div>
+
+          <div className="row text-display">
+            <div className="col-md-6 modal-subheading">
+              <Typography variant="displayText5">
+                Are you sure you want to permanently delete "{journal.title}"
+                journal.
+              </Typography>
+            </div>
+          </div>
+
+          <div className="row text-display">
+            <div className="col-md-6">
+              <Typography variant="contentText1">
+                You can't undo this action.
+              </Typography>
+            </div>
+          </div>
+        </div>
       </Modal>
     </>
   )
