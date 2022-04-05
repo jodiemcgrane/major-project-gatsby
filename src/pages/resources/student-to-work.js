870d71c02da52e4848edfa3381f8d8780bd7bc44
Jodie Mcgrane <48021738+jodiemcgrane@users.noreply.github.com>
2022-04-05 18:52:48 +0100

    Added content to resources section

--- src/pages/resources/student-to-work.js
+++ src/pages/resources/student-to-work.js
@@ -41,7 +41,43 @@ const StudentToWork = () => {
           </div>
         </div>
 
-        <div className="page"></div>
+        <div className="page">
+          <div className="section">
+            <div className="row">
+              <div className="col-md-7">
+                <Typography variant="displayText3">
+                  10 Tips to Help Transiton from Student to Profressional Life
+                </Typography>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-7 tips-heading">
+                <Typography variant="displayText6">1. Communication</Typography>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-8 tips-paragraph">
+                <Typography variant="contentText1">
+                  If you need more time to finish a project or need more
+                  guidance on your, communicate with your team.Communicating
+                  with your team and amnager will alleviate some of the anxierty
+                  and stress of starting your work here as an intern. Don't be
+                  afraid to ask for help if you need it. On the flip side, if
+                  you find that you have underestimated your workload, consult
+                  with your team to find more work, as you know they are a
+                  talented team of profressionals!
+                </Typography>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-7 tips-heading">
+                <Typography variant="displayText6">2. Communication</Typography>
+              </div>
+            </div>
+          </div>
+        </div>
       </Layout>
     </div>
   )
